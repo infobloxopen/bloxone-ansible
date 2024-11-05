6fe3c43e92bd6ab006195f16de5939954009ed36
JchhatbarInfoblox <jchhatbar@infoblox.com>
2024-11-05 11:14:35 +0530

    removed module deprecation from main.

--- plugins/modules/b1_ipam_fixed_address_gather.py
+++ plugins/modules/b1_ipam_fixed_address_gather.py
@@ -123,11 +123,6 @@ def main():
     choice_map = {"gather": get_fixed_address}
 
     module = AnsibleModule(argument_spec=argument_spec)
-    module.deprecate(
-        "This module is being deprecated and will be removed in version 3.0.0. Use the M(ipam_fixed_address_info) module instead.",
-        version="3.0.0",
-        collection_name="infoblox.bloxone",
-    )
     (is_error, has_changed, result) = choice_map.get(module.params["state"])(module.params)
 
     if not is_error:
