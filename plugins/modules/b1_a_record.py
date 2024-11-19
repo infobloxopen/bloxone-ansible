65a7df4eb92029e3ab7d5cab57b405ff90530e36
Josh Eisenbath <Josh.Eisenbath@panerabread.com>
2024-11-19 14:29:20 -0600

    allow updating of address. set api_key param to no_log to not expose in output.

--- plugins/modules/b1_a_record.py
+++ plugins/modules/b1_a_record.py
@@ -166,7 +166,7 @@ def get_a_record(data):
         return connector.get("/api/ddi/v1/dns/record")
 
 
-def update_a_record(data):
+def update_a_record_name(data):
     """Updates the existing BloxOne DDI DNS Authoritative Zone object"""
     connector = Request(data["host"], data["api_key"])
     helper = Utilities()
@@ -206,18 +206,37 @@ def update_a_record(data):
     return connector.update(endpoint, payload)
 
 
+def update_a_record(connector, data, reference):
+    """Updates the existing BloxOne DDI DNS Authoritative Zone object"""
+    helper = Utilities()
+    payload = {}
+    old_data = reference[2]["results"][0]
+    ref_id = old_data["id"]
+    if old_data["rdata"]["address"] != data["address"]:
+        payload["rdata"] = {"address": data["address"]}
+    endpoint = f"/api/ddi/v1/{ref_id}"
+    if payload:
+        return connector.update(endpoint, payload)
+    else:
+        return False
+
+
 def create_a_record(data):
     """Creates a new BloxOne DDI DNS Authoritative Zone object"""
     connector = Request(data["host"], data["api_key"])
     helper = Utilities()
     if all(k in data and data[k] is not None for k in ("name", "zone")):
         if "new_name" in data["name"]:
-            return update_a_record(data)
+            return update_a_record_name(data)
         else:
             auth_zone = get_a_record(data)
             payload = {}
             if "results" in auth_zone[2].keys() and len(auth_zone[2]["results"]) > 0:
-                return auth_zone
+                updated = update_a_record(connector, data, auth_zone)
+                if updated:
+                    return updated
+                else:
+                    return auth_zone
             else:
                 zone_endpoint = f"/api/ddi/v1/dns/auth_zone?_filter=fqdn==\"{data['zone']}\""
                 zone = connector.get(zone_endpoint)
@@ -279,7 +298,7 @@ def main():
     """Main entry point for module execution"""
     argument_spec = dict(
         zone=dict(type="str"),
-        api_key=dict(required=True, type="str"),
+        api_key=dict(required=True, type="str", no_log=True),
         host=dict(required=True, type="str"),
         name=dict(type="str"),
         address=dict(type="str"),
