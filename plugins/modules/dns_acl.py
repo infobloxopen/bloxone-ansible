4499962d55fc70be1e5d41d6758ec6807e3a8c3e
JchhatbarInfoblox <jchhatbar@infoblox.com>
2025-01-31 14:17:49 +0530

    fixed lint issues.

--- plugins/modules/dns_acl.py
+++ plugins/modules/dns_acl.py
@@ -276,8 +276,8 @@ item:
 from ansible_collections.infoblox.bloxone.plugins.module_utils.modules import BloxoneAnsibleModule
 
 try:
-    from universal_ddi_client import ApiException, NotFoundException
     from dns_config import ACL, AclApi
+    from universal_ddi_client import ApiException, NotFoundException
 except ImportError:
     pass  # Handled by BloxoneAnsibleModule
 
--- plugins/modules/dns_acl_info.py
+++ plugins/modules/dns_acl_info.py
@@ -189,8 +189,8 @@ objects:
 from ansible_collections.infoblox.bloxone.plugins.module_utils.modules import BloxoneAnsibleModule
 
 try:
-    from universal_ddi_client import ApiException, NotFoundException
     from dns_config import AclApi
+    from universal_ddi_client import ApiException, NotFoundException
 except ImportError:
     pass  # Handled by BloxoneAnsibleModule
 
